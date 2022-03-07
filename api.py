2204ff26ae20f1d8def4b1c4b78385034d49bbe0
Vennal Venkatraman <vennalmicheals@gmail.com>
2022-03-07 21:08:31 +0530

    first commit

--- api.py
+++ api.py
@@ -5,6 +5,8 @@ import pandas as pd
 import numpy as np
 import pickle
 import warnings
+
+
 warnings.filterwarnings("ignore")
 # import xgboost
 
@@ -13,10 +15,16 @@ app = Flask(__name__)
 
 with open('Model/model.pkl','rb') as fp:
 	model = pickle.load(fp)
+with open('Model/lr_model.pkl','rb') as fp:
+	lr_model = pickle.load(fp)
+with open('Model/tf_idf_vectorizermodel.pkl','rb') as fp:
+	tf_idf_model = pickle.load(fp)
+with open('Model/recommendation_model.pkl','rb') as fp:
+	recommend_model = pickle.load(fp)
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+	return render_template('index_recommend.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
@@ -27,6 +35,17 @@ def predict():
 	Input = [[SL,SW,PL,PW]]
 	prediction = model.predict(Input)[0]
 	return render_template('index.html', OUTPUT=str(prediction))
+
+@app.route('/recommend',methods=['POST'])
+def recommend():
+	user_name = request.form.get('user_name')
+	products_list = recommend_model.loc[user_name].sort_values(ascending=False)[0:20]
+	products = []
+	for product in products_list.keys():
+		products.append(product)
+
+
+	return render_template('index_recommend.html',OUTPUT = products)
 
 if __name__ == "__main__":
     app.run()
